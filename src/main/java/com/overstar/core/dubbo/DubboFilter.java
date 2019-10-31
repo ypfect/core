49b97d10db4aca8ff84593ff9f42b099ae1895b8
ypfect <363825455@qq.com>
2019-11-01 00:05:24 +0800

    bug fixed

--- src/main/java/com/overstar/core/dubbo/DubboFilter.java
+++ src/main/java/com/overstar/core/dubbo/DubboFilter.java
@@ -1,36 +1,36 @@
-package com.overstar.core.dubbo;
-
-import com.alibaba.fastjson.JSON;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.dubbo.rpc.*;
-
-/**
- * @Description
- * @Author stanley.yu
- * @Date 2019/9/24 21:30
- */
-@Slf4j
-public class DubboFilter implements Filter {
-    @Override
-    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        Object[] args = invocation.getArguments();
-        String argsJson = JSON.toJSONString(args);
-        long start = System.currentTimeMillis();
-        try {
-            return invoker.invoke(invocation);
-        } catch (RpcException re) {
-            log.error("rpc error, args:[ {} ].", argsJson, re);
-            throw re;
-        } catch (Throwable e) {
-            log.error(" rpc error, args:[ {} ].", argsJson, e);
-            throw new RpcException("Dubbo server uncaught exception!", e);
-        } finally {
-            long spend = (System.currentTimeMillis() - start);
-            int clientPort = RpcContext.getContext().getRemotePort();
-            // 获取调用方IP地址
-            String clientIP = RpcContext.getContext().getRemoteHost();
-            log.info("DubboRPC info: API[ {} ], method[ {} ], args[ {} ], spendTime[ {} ms ], clientIp[ {} ], clientPort[ {} ].",
-                    invoker.getInterface().getSimpleName(), invocation.getMethodName(), argsJson, spend, clientIP, clientPort);
-        }
-    }
-}
+//package com.overstar.core.dubbo;
+//
+//import com.alibaba.fastjson.JSON;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.dubbo.rpc.*;
+//
+///**
+// * @Description
+// * @Author stanley.yu
+// * @Date 2019/9/24 21:30
+// */
+//@Slf4j
+//public class DubboFilter implements Filter {
+//    @Override
+//    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+//        Object[] args = invocation.getArguments();
+//        String argsJson = JSON.toJSONString(args);
+//        long start = System.currentTimeMillis();
+//        try {
+//            return invoker.invoke(invocation);
+//        } catch (RpcException re) {
+//            log.error("rpc error, args:[ {} ].", argsJson, re);
+//            throw re;
+//        } catch (Throwable e) {
+//            log.error(" rpc error, args:[ {} ].", argsJson, e);
+//            throw new RpcException("Dubbo server uncaught exception!", e);
+//        } finally {
+//            long spend = (System.currentTimeMillis() - start);
+//            int clientPort = RpcContext.getContext().getRemotePort();
+//            // 获取调用方IP地址
+//            String clientIP = RpcContext.getContext().getRemoteHost();
+//            log.info("DubboRPC info: API[ {} ], method[ {} ], args[ {} ], spendTime[ {} ms ], clientIp[ {} ], clientPort[ {} ].",
+//                    invoker.getInterface().getSimpleName(), invocation.getMethodName(), argsJson, spend, clientIP, clientPort);
+//        }
+//    }
+//}
